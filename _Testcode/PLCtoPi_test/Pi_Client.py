9e6320dd8fd2cb55e374b3cd7c3bcdebd1ecc8f0
Jay Lee <lyj911111@naver.com>
2019-04-25 10:09:20 +0700

    txt파일 여러개 있어도 모두 읽어서 전송

--- _Testcode/PLCtoPi_test/Pi_Client.py
+++ _Testcode/PLCtoPi_test/Pi_Client.py
@@ -12,11 +12,11 @@ folderlist = os.listdir(acess_location)         # Save as lists at folder on pat
 
 
 # Return the lines[list] in text file through indexing Folder.
-def read_file(index):
+def read_file(index, numberOfFileindex):
     global acess_location, folderlist
 
-    filename = os.listdir(acess_location + folderlist[0])                 # Save the filename from list.
-    filetext = acess_location + folderlist[index] + '/' + filename[0]     # "path + .txt" file for using read the file
+    filename = os.listdir(acess_location + folderlist[index])                 			# Save the filename from list.
+    filetext = acess_location + folderlist[index] + '/' + filename[numberOfFileindex]   # "path + .txt" file for using read the file
 
     try:
         f = open(filetext, 'r')                                               # Open as read mode
@@ -61,28 +61,33 @@ def runChat():
             print("Number of Folder: ", len(folderlist))
             if folderlist != 0:
 
+                # number of folders
                 for i in range(len(folderlist)):
-                    lines, filetext = read_file(i)  # return lines[list], .txt file path
+                    filename = os.listdir(acess_location + folderlist[i])
 
-                    if lines != None:
-                        for line in lines:
+                    # number of .txt files
+                    for j in range(len(filename)):
+                        lines, filetext = read_file(i, j)  # return lines[list], .txt file path
 
-                            # if the text file has only 1 line, Remove it, cuz it already sent the message to Server.
-                            if len(lines) > 1:
-                                sock.send(line.encode())  # Send the message per reading
-                            else:
-                                sock.send(line.encode())        # Send the message per reading when it lower than 1 line
-                                line = lastlineProcess(line)    # Remove only one line
-                                f = open(filetext, 'w')
-                                f.write(line)
-                                f.close()
-                            # cv2.waitKey(100)
-
-                            print("file path : ", filetext)
-                            print("remove line : ", line)
-                            with open(filetext, 'w') as f:      # remove the line after reading
-                                if line.strip('\n') != line:
+                        if lines != None:
+                            for line in lines:
+    
+                                # if the text file has only 1 line, Remove it, cuz it already sent the message to Server.
+                                if len(lines) > 1:
+                                    sock.send(line.encode())  # Send the message per reading
+                                else:
+                                    sock.send(line.encode())        # Send the message per reading when it lower than 1 line
+                                    line = lastlineProcess(line)    # Remove only one line
+                                    f = open(filetext, 'w')
                                     f.write(line)
+                                    f.close()
+                                # cv2.waitKey(100)
+
+                                print("file path : ", filetext)
+                                print("remove line : ", line)
+                                with open(filetext, 'w') as f:      # remove the line after reading
+                                    if line.strip('\n') != line:
+                                        f.write(line)
 
             else:
                 print("empty the folders")
